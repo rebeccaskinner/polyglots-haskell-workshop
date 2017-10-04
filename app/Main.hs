aeab53352da33784dd405f5a1c28e9e42cd9ccd6
Rebecca Skinner <rebecca@rebeccaskinner.net>
2017-10-04 18:02:23 -0500

    prepare for port number refactor

--- app/Main.hs
+++ app/Main.hs
@@ -8,16 +8,16 @@ import Data.Maybe (listToMaybe, fromMaybe)
 import System.IO.Error (catchIOError)
 
 main :: IO ()
-main = do
-  port <- fromMaybe 8080 <$> getPortParam
-  runApp port
+main = runApp 8080
 
+-- getPortParam tries to get a parameter from the command line
 getPortParam :: IO (Maybe Int)
 getPortParam = do
   hd <- listToMaybe <$> getArgs
-  case hd of
-    Nothing -> return Nothing
-    Just hd' -> readMaybe hd'
+  return $ case hd of
+    Nothing  -> Nothing
+    Just hd' -> Just 8080
 
+-- readMaybe tries to parse a string into a readable type
 readMaybe :: Read a => String -> IO (Maybe a)
 readMaybe s = catchIOError (Just <$> readIO s) (\_ -> return Nothing)
