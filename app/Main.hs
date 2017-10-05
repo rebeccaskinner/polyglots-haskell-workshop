d4f2674c2f9d37882b7f547d2c00bdbdf75d2840
Rebecca Skinner <rebecca@rebeccaskinner.net>
2017-10-05 09:54:08 -0500

    exercise 1: parameterize port

--- app/Main.hs
+++ app/Main.hs
@@ -6,7 +6,9 @@ import Text.Read (readMaybe)
 import Control.Monad ((>=>))
 
 main :: IO ()
-main = runApp 8080
+main = do
+  port <- fromMaybe 8080 <$> getPortParam
+  runApp port
 
 -- getPortParam tries to get a parameter from the command line
 getPortParam :: IO (Maybe Int)
